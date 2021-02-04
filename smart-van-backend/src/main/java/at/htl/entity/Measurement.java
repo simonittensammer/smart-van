138114fb9151aa2fcd95a83a5005516040463db9
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 15:25:03 +0100

    add attributes to class Measurement

--- smart-van-backend/src/main/java/at/htl/entity/Measurement.java
+++ smart-van-backend/src/main/java/at/htl/entity/Measurement.java
@@ -1,4 +1,62 @@
 package at.htl.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Measurement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double temp;
+
+    private LocalDateTime timeStamp;
+
+    private Long sensorId;
+
+    public Measurement() {
+    }
+
+    public Measurement(double temp, LocalDateTime timeStamp, Long sensorId) {
+        this.temp = temp;
+        this.timeStamp = timeStamp;
+        this.sensorId = sensorId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(double temp) {
+        this.temp = temp;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Long getSensorId() {
+        return sensorId;
+    }
+
+    public void setSensorId(Long sensorId) {
+        this.sensorId = sensorId;
+    }
 }
