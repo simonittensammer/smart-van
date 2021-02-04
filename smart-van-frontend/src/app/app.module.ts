4bc1063960c908feb0dda84aa58b9c4f1ea7c5a4
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 15:56:03 +0100

    add angular material

--- smart-van-frontend/src/app/app.module.ts
+++ smart-van-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MeasurementOverviewComponent } from './measurement-overview/measurement-overview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MeasurementOverviewComponent } from './measurement-overview/measurement
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
