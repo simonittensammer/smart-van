1db318d0fb2df2c6bfd5315a823d0486d656ac21
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 17:46:10 +0100

    add toolbar

--- smart-van-frontend/src/app/app.module.ts
+++ smart-van-frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,18 +7,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { MeasurementOverviewComponent } from './measurement-overview/measurement-overview.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
+import { MeasurementBarChartComponent } from './measurement-bar-chart/measurement-bar-chart.component';
+import { ChartsModule } from 'ng2-charts';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MeasurementOverviewComponent
+    MeasurementOverviewComponent,
+    MeasurementBarChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatTableModule
+    ChartsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
