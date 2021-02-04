cddc37625bfc2d9bb9ec2f02653ec1af14b564cd
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 15:33:12 +0100

    add getAll to MeasurementEndpoint

--- smart-van-backend/src/main/java/at/htl/boundary/MeasurementEndpoint.java
+++ smart-van-backend/src/main/java/at/htl/boundary/MeasurementEndpoint.java
@@ -1,4 +1,31 @@
 package at.htl.boundary;
 
+import at.htl.control.MeasurementRepository;
+import at.htl.entity.Measurement;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/measurement")
+@ApplicationScoped
+@Transactional
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MeasurementEndpoint {
+
+    @Inject
+    MeasurementRepository mr;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public List<Measurement> getAll() {
+        return mr.listAll();
+    }
 }
