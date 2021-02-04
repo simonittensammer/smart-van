6c205e9a2e717c6feb5d2cd94d450b63b3b7ac56
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 17:50:09 +0100

    create measurement-chart component with basic functionality

--- smart-van-frontend/src/app/app.module.ts
+++ smart-van-frontend/src/app/app.module.ts
@@ -11,12 +11,14 @@ import { MeasurementBarChartComponent } from './measurement-bar-chart/measuremen
 import { ChartsModule } from 'ng2-charts';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MeasurementOverviewComponent,
-    MeasurementBarChartComponent
+    MeasurementBarChartComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
