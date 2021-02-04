8592cb38f4d96f49254043f4b39b58a0feda2c85
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 15:46:52 +0100

    add attributes to model Measurement

--- smart-van-frontend/src/app/models/measurement.ts
+++ smart-van-frontend/src/app/models/measurement.ts
@@ -1,2 +1,14 @@
 export class Measurement {
+
+  id: number;
+  temp: number;
+  timeStamp: Date;
+  sensorId: number;
+
+  constructor(id: number, temp: number, timeStamp: Date, sensorId: number) {
+    this.id = id;
+    this.temp = temp;
+    this.timeStamp = timeStamp;
+    this.sensorId = sensorId;
+  }
 }
