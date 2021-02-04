1ebd46ad061add6e24ef470ed963d33e589a92d6
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 15:37:15 +0100

    add addMeasurement endpoint to MeasurementEndpoint

--- smart-van-backend/src/main/java/at/htl/boundary/MeasurementEndpoint.java
+++ smart-van-backend/src/main/java/at/htl/boundary/MeasurementEndpoint.java
@@ -6,11 +6,9 @@ import at.htl.entity.Measurement;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/measurement")
@@ -24,8 +22,13 @@ public class MeasurementEndpoint {
     MeasurementRepository mr;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public List<Measurement> getAll() {
         return mr.listAll();
+    }
+
+    @POST
+    public Response addMeasurement(Measurement measurement) {
+        mr.persist(measurement);
+        return Response.ok(measurement).build();
     }
 }
