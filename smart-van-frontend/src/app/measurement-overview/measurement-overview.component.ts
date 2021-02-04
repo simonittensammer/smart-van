820b69ebd64b73042d64668182db3c1018f08141
simonittensammer <simon.ittensammer@gmx.at>
2021-02-04 20:39:15 +0100

    add paginator

--- smart-van-frontend/src/app/measurement-overview/measurement-overview.component.ts
+++ smart-van-frontend/src/app/measurement-overview/measurement-overview.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MeasurementService} from '../services/measurement.service';
+import {MatTableDataSource} from '@angular/material/table';
+import {Measurement} from '../models/measurement';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-measurement-overview',
   templateUrl: './measurement-overview.component.html',
   styleUrls: ['./measurement-overview.component.scss']
 })
-export class MeasurementOverviewComponent implements OnInit {
+export class MeasurementOverviewComponent implements OnInit, AfterViewInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id', 'temp', 'timeStamp', 'sensorId'];
+  dataSource: MatTableDataSource<Measurement> = new MatTableDataSource<Measurement>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(
     public measurementService: MeasurementService
@@ -17,7 +23,12 @@ export class MeasurementOverviewComponent implements OnInit {
   ngOnInit(): void {
     this.measurementService.getAllMeasurements().subscribe(value => {
       this.measurementService.measurements = value;
+      this.dataSource.data = value;
+      this.dataSource.paginator = this.paginator;
     });
+  }
+
+  ngAfterViewInit(): void {
   }
 
 }
